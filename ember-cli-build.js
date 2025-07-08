fb84a2fc72f5580a438d94597e1eebe074118723
Maarten Segers <maarten.segers@gmail.com>
2025-07-08 18:21:36 +0200

    Add styleguide

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,8 +13,7 @@ module.exports = function (defaults) {
     sassOptions: {
       includePaths: [
         'node_modules/breakpoint-sass/stylesheets',
-        'node_modules/susy/sass'
-      ]
+        'node_modules/gent_styleguide/build/styleguide'      ]
     },
   });
 
