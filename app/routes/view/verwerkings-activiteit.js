4973795e05f3ed65ee73477354f46f148e584a5e
Maarten Segers <maarten.segers@gmail.com>
2025-08-25 10:05:11 +0200

    Add HTML

--- app/routes/view/verwerkings-activiteit.js
+++ app/routes/view/verwerkings-activiteit.js
@@ -11,21 +11,42 @@ export default class ViewVerwerkingsActiviteitRoute extends Route {
 
   async model({ resource }) {
     const query = `
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX dct: <http://purl.org/dc/terms/>
-SELECT ?title ?type ?verwerker ?grond ?desc ?period
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#> 
+PREFIX dct: <http://purl.org/dc/terms/> 
+PREFIX gdv: <http://stad.gent/data/ns/data-processing/> 
+SELECT 
+  ?verwerking 
+  ?id  
+  ?description 
+  ?processor
+  ?type 
+  ?name 
+  ?personalDataDescription 
+  ?sensitivePersonalDataDescription 
+  (group_concat(distinct ?personalData;separator=',') as ?personalData) 
+  (group_concat(distinct ?sensitivePersonalData;separator=',') as ?sensitivePersonalData) 
+  ?formal_framework 
+  ?formal_framework_clarification 
+  (group_concat(distinct ?grantee;separator=',') as ?grantees) 
+  ?storagePeriod 
 WHERE {
-  <${resource}> dct:title ?title;
-                dct:type/skos:prefLabel ?type;
-                <http://data.vlaanderen.be/ns/toestemming#verwerker>/skos:prefLabel ?verwerker;
-                <http://data.vlaanderen.be/ns/toestemming#verwerkingsgrond>/skos:prefLabel ?grond.
-  OPTIONAL {
-    <${resource}> dct:description ?desc.
-  }
-  OPTIONAL {
-    <${resource}> dct:temporal/dct:title ?period.
-  }
+  <${resource}> a <http://data.vlaanderen.be/ns/toestemming#VerwerkingsActiviteit>; 
+                dct:identifier ?id; 
+                dct:description ?description; 
+                dct:type/skos:prefLabel ?type; 
+                dct:title ?name;  
+                dct:temporal/dct:title ?storagePeriod;
+                <http://data.vlaanderen.be/ns/toestemming#verwerkingsgrond>/skos:prefLabel ?formal_framework;
+                <http://data.vlaanderen.be/ns/toestemming#verwerker>/skos:prefLabel ?processor.
+  OPTIONAL { <${resource}> gdv:verduidelijkingRechtsgrond ?formal_framework_clarification }
+  OPTIONAL { <${resource}> <http://stad.gent/data/ns/data-processing/grantee>/skos:prefLabel ?grantee }
+  OPTIONAL { <${resource}> <http://stad.gent/data/ns/data-processing/hasPersonalData>/dct:type/skos:prefLabel ?personalData } 
+  OPTIONAL { <${resource}> <http://stad.gent/data/ns/data-processing/hasSensitivePersonalData>/dct:type/skos:prefLabel ?sensitivePersonalData } 
+  OPTIONAL { <${resource}> <http://stad.gent/data/ns/data-processing/hasPersonalData>/dct:description ?personalDataDescription } 
+  OPTIONAL { <${resource}> <http://stad.gent/data/ns/data-processing/hasSensitivePersonalData>/dct:description ?sensitivePersonalDataDescription } 
+  BIND(<${resource}> as ?verwerking)
 }
+GROUP BY ?verwerking ?id ?description ?processor ?type ?name ?personalDataDescription ?sensitivePersonalDataDescription ?formal_framework ?formal_framework_clarification ?storagePeriod
 `;
     console.log(query);
     let endpoint;
@@ -45,13 +66,24 @@ WHERE {
     const bindings = data.results.bindings[0] ?? {};
 
     const getValue = (field) => bindings[field]?.value || '';
+    const getValueSplit = (field) => {
+      const value = getValue(field);
+      return value ? value.split(',') : [];
+    };
     return {
+      id: getValue('id'),
+      title: getValue('name'),
       categorie: getValue('type'),
-      title: getValue('title'),
-      verwerkers: getValue('verwerker'),
-      rechtmatigheid: getValue('grond'),
-      beschrijving: getValue('desc'),
-      bewaartermijn: getValue('period'),
+      verwerkers: getValue('processor'),
+      rechtmatigheid: getValue('formal_framework'),
+      rechtmatigheidVerduidelijking: getValue('formal_framework_clarification'),
+      beschrijving: getValue('description'),
+      bewaartermijn: getValue('storagePeriod'),
+      persoonsgegevens: getValueSplit('personalData'),
+      persoonsgegevensBeschrijving: getValue('personalDataDescription'),
+      gevoeligePersoonsgegevens: getValueSplit('sensitivePersonalData'),
+      gevoeligePersoonsgegevensBeschrijving: getValue('sensitivePersonalDataDescription'),
+      toegangVerleend: getValueSplit('grantees'),
       resource: resource
     };
   }
