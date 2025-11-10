e971fbd70d05649d53cbff18b98af018398d0ab5
Maarten Segers <maarten.segers@district09.gent>
2025-11-10 14:30:54 +0100

    Add styling

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,16 +3,9 @@ import { service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
   @service intl;
-  @service('dynamic-styles') dynamicStyles;
 
   beforeModel() {
     // metis uses ember-intl, but defaults to en-us
     this.intl.setLocale(['nl-be']);
-  }
-
-  activate() {
-    super.activate(...arguments);
-    // Initialize dynamic stylesheet loading
-    this.dynamicStyles.updateStylesheet();
   }
 }
