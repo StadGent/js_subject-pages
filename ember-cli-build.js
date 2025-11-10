ebbfd9b9a1b027eed522918ee3f5c345ac5128ce
Maarten Segers <maarten.segers@district09.gent>
2025-11-10 15:43:52 +0100

    Build styleguide

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,6 +22,27 @@ module.exports = function (defaults) {
   // return app.toTree();
 
   const { Webpack } = require('@embroider/webpack');
+
+  // Copy Gent styleguide assets during build
+  const path = require('path');
+  const fs = require('fs-extra');
+
+  const styleguideSource = path.join(__dirname, 'node_modules/gent_styleguide/build/styleguide');
+  const assetsDestination = path.join(__dirname, 'public/assets');
+
+  // Copy all gent_styleguide assets (fonts, googlefonts, img, js)
+  const assetFolders = ['fonts', 'googlefonts', 'img', 'js'];
+
+  assetFolders.forEach(folder => {
+    const sourceFolder = path.join(styleguideSource, folder);
+    const destFolder = path.join(assetsDestination, folder);
+
+    if (fs.existsSync(sourceFolder)) {
+      fs.ensureDirSync(destFolder);
+      fs.copySync(sourceFolder, destFolder);
+    }
+  });
+
   return require('@embroider/compat').compatBuild(app, Webpack, {
     staticAddonTestSupportTrees: true,
     staticAddonTrees: true,
