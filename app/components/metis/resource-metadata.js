a44075241b6bb48a27613d043e638d3a2e79a450
Maarten Segers <maarten.segers@district09.gent>
2025-11-17 10:09:05 +0100

    LOD-374 Fix translations

---
+++ app/components/metis/resource-metadata.js
@@ -0,0 +1,6 @@
+import MetisResourceMetadataComponent from 'ember-metis/components/metis/resource-metadata';
+
+export default class ResourceMetadataComponent extends MetisResourceMetadataComponent {
+  // Inherit all functionality from parent component
+  // This provides the custom 't' helper that prefixes translations with 'fallback.' or 'external.'
+}
